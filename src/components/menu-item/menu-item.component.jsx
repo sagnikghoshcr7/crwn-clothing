7d35c78f5ff5cbd239ecf72cbc9dc46546fe6cb5
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-22 03:16:52 +0530

    add component background images

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl }) => (
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }} className='menu-item'>
         <div className='content'>
         <div className='title'>{title}</div>
         <span className='subtitle'>SHOP NOW</span>
