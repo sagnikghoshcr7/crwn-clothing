20f7b4bce57768d590f174486fc1b89472c1ff92
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-21 20:12:05 +0530

    initial functionalities to homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+      <div classname='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+)
