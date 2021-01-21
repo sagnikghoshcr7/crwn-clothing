aee1b0fccffd9fa46cdb0e345d148de587480656
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-21 20:23:34 +0530

    add sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
   return (
     <div className="App">
-      Sagnik Ghosh
+      <HomePage />
     </div>
   );
 }
