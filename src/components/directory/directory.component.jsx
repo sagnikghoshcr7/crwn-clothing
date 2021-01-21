4db4577128e423e639b78daca8f38acecee43fb1
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-22 03:24:37 +0530

    update lower background images to big

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,8 +49,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ title, imageUrl, id }) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} />
+                    this.state.sections.map(({ title, imageUrl, id, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                     ))
                 }
             </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl }) => (
+const MenuItem = ({ title, imageUrl, size }) => (
     <div style={{
         backgroundImage: `url(${imageUrl})`
-    }} className='menu-item'>
+    }} className={`${size} menu-item`}>
         <div className='content'>
         <div className='title'>{title}</div>
         <span className='subtitle'>SHOP NOW</span>
