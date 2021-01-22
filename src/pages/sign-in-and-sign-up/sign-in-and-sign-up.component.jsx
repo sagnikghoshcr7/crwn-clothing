ea276f81339f69809343e3b94107e785fac49e6b
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-23 03:21:31 +0530

    add sign in page component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+  <div className='sign-in-and-sign-up'>SIGN IN</div>
+);
+
+export default SignInAndSignUpPage;
