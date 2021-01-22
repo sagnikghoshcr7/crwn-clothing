e3e71ea5aa52dd8a4683d0fc6cc795ef0cf92b05
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-23 02:21:05 +0530

    update collection tem component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview';
+
 class ShopPage extends React.Component {
     constructor(props) {
         super(props);
@@ -12,7 +14,14 @@ class ShopPage extends React.Component {
     }
 
     render() {
-        return <div>SHOP PAGE</div>
+        const {collections} = this.state;
+        return (<div className='shop-page'>
+            {
+                collections.map(({ id, ...otherCollectionProps }) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))
+            }
+        </div>);
     }
 }
 
