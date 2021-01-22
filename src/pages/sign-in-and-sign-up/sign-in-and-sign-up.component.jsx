d3e91162c5162627c5c42d06e6c929d1b417a3c3
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-23 03:48:21 +0530

    update sign in page component

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import SignIn from '../../components/sign-in/sign-in.component';
+
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-  <div className='sign-in-and-sign-up'>SIGN IN</div>
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
 );
 
 export default SignInAndSignUpPage;
