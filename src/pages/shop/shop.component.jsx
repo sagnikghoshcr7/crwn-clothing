ebc177b8a950492105a25e38bebfa2a8ab906f60
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-22 22:17:38 +0530

    add SHOP_PAGE

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data.js';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        };
+    }
+
+    render() {
+        return <div>SHOP PAGE</div>
+    }
+}
+
+export default ShopPage;
