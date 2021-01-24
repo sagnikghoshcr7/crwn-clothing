98cee6ea3c8090d96f58e301dd4131c73224d2b6
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-25 03:32:13 +0530

    add redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  <Provider>
     <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
+      <App />
+    </BrowserRouter>      
+  </Provider>,
+  document.getElementById('root')
 );
 
 
