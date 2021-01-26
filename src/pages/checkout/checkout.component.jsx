4f384cc80299315b756b7179b46e0b208f6357c1
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-27 02:54:03 +0530

    update checkout-page by adding total cost

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
-const CheckoutPage = () => (
+const CheckoutPage = ({cartItems, total}) => (
   <div className='checkout-page'>
     <div className='checkout-header'>
       <div className='header-block'>
@@ -21,7 +25,20 @@ const CheckoutPage = () => (
         <span>Remove</span>
       </div>
     </div>
+    {
+      cartItems.map(cartItem =>
+        cartItem.name
+      )
+    }
+    <div className='total'>
+      <span>TOTAL: ${total}</span>
+    </div>
   </div>
 );
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
