684217aa3b8a9a37fa04c3217fbab2917facdeb9
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-27 01:59:20 +0530

    update cart-dropdown by adding no items in the cart message

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -12,8 +12,13 @@ const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
     <div className='cart-items' >
       {
-        cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
-      }
+        cartItems.length ? (
+        cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>   
+      )}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
