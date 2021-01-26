639cc04e6e58e064143c3786e4c272bbf498cdf4
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-27 02:40:03 +0530

    update checkout-page-component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,8 +3,24 @@ import React from 'react';
 import './checkout.styles.scss';
 
 const CheckoutPage = () => (
-  <div>
-    CHECKOUT PAGE
+  <div className='checkout-page'>
+    <div className='checkout-header'>
+      <div className='header-block'>
+        <span>Product</span>
+      </div>
+      <div className='header-block'>
+        <span>Description</span>
+      </div>
+      <div className='header-block'>
+        <span>Quantity</span>
+      </div>
+      <div className='header-block'>
+        <span>Price</span>
+      </div>
+      <div className='header-block'>
+        <span>Remove</span>
+      </div>
+    </div>
   </div>
 );
 
