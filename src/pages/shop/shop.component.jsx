0302c334de180f1c7ab62c5ff49cea50291d7021
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-28 22:07:46 +0530

    add category component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.components';
+import CategoryPage from '../category/category.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
   </div>
 );
 
