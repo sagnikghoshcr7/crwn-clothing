c351b70339e8599707f7f1aed4f4f617d4942105
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-29 00:36:43 +0530

    add advanced collection routing

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
   return (
     <div className='collection-page'>
       <h2>COLLECTION PAGE</h2>
@@ -13,4 +16,8 @@ const CollectionPage = ({ match }) => {
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
